951a8cea7b840bce367a1d659c8402a67dfe8d73
Stephen <syangrea@gmail.com>
2021-01-05 15:45:24 -0500

    up to event handler for tabs

--- skeleton/frontend/tab.jsx
+++ skeleton/frontend/tab.jsx
@@ -5,21 +5,31 @@ export default class Tab extends React.Component{
     constructor(props){
         super(props);
         this.state = {currentTab: 0}
+        this.selectedTab = this.selectedTab.bind(this);
+    }
+
+    selectedTab(event){
+        event.preventDefault();
+        this.setState({currentTab: event.currentTarget.key})
     }
 
     render(){
-        const header = this.props.tabs.map = (el,idx) => {
+        const header = this.props.tabs.map((el,idx) => {
             if (idx === this.state.currentTab){
-                return <h1 className="current">{el.title}</h1>
+                return <li onClick={this.selectedTab} key={idx}><h1 className="current">{el.title}</h1></li>
             } else {
-                return <h1>{el.title}</h1>
+                return <li onClick={this.selectedTab} key={idx}><h1>{el.title}</h1></li>
             }
-        };
+        });
         
         return (
             <div className="tab">
-                <header>{header}</header>
-                <article>{this.props.tabs[this.state.currentTab].content} </article>
+                <header>
+                    <ul>
+                        {header}
+                    </ul>
+                </header>
+                <article>{(this.props.tabs[this.state.currentTab])} </article>
             </div>
         )
     }
